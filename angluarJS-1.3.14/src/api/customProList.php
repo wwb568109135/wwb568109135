fb6df8d6798cd56a1efc730563ef295e4f92bd10
jerry.wu <568109135@qq.com>
2016-10-20 07:53:01 +0800

    Add files via upload

---
+++ angluarJS-1.3.14/src/api/customProList.php
@@ -0,0 +1,11 @@
+getCollectionListCallback({
+    "code" : 1,
+    "data" : [
+        {
+            "goodsId": 59345659,
+            "goodsName": "cnbcb"
+        }
+    ],
+    "result" : true,
+    "msg" : "获取尺码列表成功!"
+})
---
+++ angluarJS-1.3.14/src/api/stock.php
@@ -0,0 +1,6 @@
+callbackSoldOut({
+	code : 1,
+	msg : "获取成功！",
+	data : [20039241],
+	result : true
+});
